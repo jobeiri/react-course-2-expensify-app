6b61b55a1a78831aa2a92e65aebc5ee58bcaeae8
Amir Hossein Jobeiri <jobeiri_a.h@yahoo.com>
2020-09-17 21:25:40 -0700

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,64 @@
-const path = require('path');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { TRUE } = require("node-sass");
 
-module.exports = {
+module.exports = (env) => {
+  const isProduction = env === "production";
+  console.log("env", env);
+  return {
     entry: "./src/app.js",
     output: {
-        path: path.join(__dirname, 'public'),
-        filename: "bundle.js"
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
     },
     module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          /*
+          use: [
+            new MiniCssExtractPlugin(),
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+              },
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+          */
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                sourceMap: true,
+              },
+            },
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+      ],
     },
-    devtool: 'eval-cheap-module-source-map',
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "styles.css",
+      }),
+    ],
+    devtool: isProduction ? "source-map" : "inline-source-map",
     devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true,
+    },
+  };
 };
