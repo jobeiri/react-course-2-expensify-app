a51bbef8253a8003099b9a154103c9c4b5f70199
Amir Hossein Jobeiri <jobeiri_a.h@yahoo.com>
2020-10-24 03:04:43 -0700

    7. Styling Expense Form

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { startLogin } from "../actions/auth";
 
 export const LoginPage = ({ startLogin }) => (
-  <div>
-    <button onClick={startLogin}>Login with Google</button>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1>Expensify</h1>
+      <p>It's time to get your expenses under control.</p>
+      <button className="button" onClick={startLogin}>
+        Login with Google
+      </button>
+    </div>
   </div>
 );
 
